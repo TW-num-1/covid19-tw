48e08e284b332da27ceed6e1abefa6ba1a5de6f4
Kelvin Kwong Ho <kelvinho84@gmail.com>
2020-03-19 15:52:22 +0800

    add

--- getData.sh
+++ getData.sh
@@ -6,10 +6,7 @@ curl -X GET \
   "https://sheets.googleapis.com/v4/spreadsheets/1I9EXxe-pWLhcLosakg5TPt98ERY6tdpJn1KngIGY7oY/values/%E7%A2%BA%E8%A8%BA%E7%97%85%E4%BE%8B!A1:I?majorDimension=ROWS&key=$1" \
   -o latest.json
 
-if git diff-index --quiet HEAD --; then
-    # No changes
-    echo 'no'
-else
+if [[ `git status --porcelain` ]]; then
     # Changes
     echo 'change'
 fi
