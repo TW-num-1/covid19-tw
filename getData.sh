e8d86ce783e1c2a708c8485ffe39d5327ed4ced0
kelvin-pi <kelvinho84@gmail.com>
2020-03-19 16:52:02 +0800

    03-19-2020-165201

--- getData.sh
+++ getData.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 TS=`date +"%m-%d-%Y-%H%M%S"`;
-
+whoami 
 echo ${TS};
 curl -X GET \
   "https://sheets.googleapis.com/v4/spreadsheets/1I9EXxe-pWLhcLosakg5TPt98ERY6tdpJn1KngIGY7oY/values/%E7%A2%BA%E8%A8%BA%E7%97%85%E4%BE%8B!A1:I?majorDimension=ROWS&key=$1" \
