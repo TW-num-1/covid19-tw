9a3a2d18b9ce6195c86fba6cb1c3f1bf320c2919
Kelvin Kwong Ho <kelvinho84@gmail.com>
2020-04-26 09:12:00 +0800

    Update getData.sh

--- getData.sh
+++ getData.sh
@@ -7,7 +7,7 @@ whoami
 echo ${TS};
 
 curl -X GET \
-  "https://sheets.googleapis.com/v4/spreadsheets/1I9EXxe-pWLhcLosakg5TPt98ERY6tdpJn1KngIGY7oY/values/%E7%A2%BA%E8%A8%BA%E7%97%85%E4%BE%8B!A1:L?majorDimension=ROWS&key=$1" \
+  "https://sheets.googleapis.com/v4/spreadsheets/1I9EXxe-pWLhcLosakg5TPt98ERY6tdpJn1KngIGY7oY/values/%E7%A2%BA%E8%A8%BA%E7%97%85%E4%BE%8B!A1:M?majorDimension=ROWS&key=$1" \
   -o latest.json
 
 if [[ `git status --porcelain` ]]; then
