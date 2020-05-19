be249aeba342f14de715c3b173bb5e0b28413d2a
Kelvin Ho <kelvin_ho@ubiqconn.com>
2020-05-19 12:11:09 +0800

    05-19-2020-121106

--- getData.sh
+++ getData.sh
@@ -3,8 +3,10 @@ TS=`date +"%m-%d-%Y-%H%M%S"`;
 
 git pull;
 
-whoami 
-echo ${TS};
+name=$(whoami)
+
+echo "The current useris $name";
+echo "Current timestamp: $TS";
 
 curl -X GET \
   "https://sheets.googleapis.com/v4/spreadsheets/1I9EXxe-pWLhcLosakg5TPt98ERY6tdpJn1KngIGY7oY/values/%E7%A2%BA%E8%A8%BA%E7%97%85%E4%BE%8B!A1:M?majorDimension=ROWS&key=$1" \
@@ -16,6 +18,8 @@ if [[ `git status --porcelain` ]]; then
     git add -A;
     git commit -m "${TS}";
     git push origin master;
+else 
+    echo 'Nothing to update'
 fi
 
 
